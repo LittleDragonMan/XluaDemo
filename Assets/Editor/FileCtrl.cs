0bd60d600b39fc36da9e6f99693c833c8dbb7e86
clc <clc@appcup.com>
2017-02-28 18:45:19 +0800

    no message

---
+++ Assets/Editor/FileCtrl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class FileCtrl : Editor {
+    [MenuItem ("TestFile/File Move")]
+    private static void FileMove() {
+        string searchPath = Application.dataPath + "/XLua/Examples";
+        string[] dirs = Directory.GetFiles(searchPath, "*.txt", SearchOption.AllDirectories);
+        Debug.Log("dirs.Count--------:"+dirs.Length);
+        foreach(string str in dirs){
+           
+        }
+    }
+	
+}
